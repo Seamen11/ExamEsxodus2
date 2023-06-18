a59751a2c00b80b3ba872f3161ff332755491abd
Seamen11 <mnu704@mail.ru>
2023-06-18 14:59:07 +0300

    Zadanie14

---
+++ src/zadanie14/Pryamougolnyi.java
@@ -0,0 +1,25 @@
+package zadanie14;
+
+import java.util.Scanner;
+
+public class Pryamougolnyi extends Treugolnik
+{
+    public static boolean pryamougolnyi(int a, int b, int c)
+    {
+        return b * b + c * c == a * a;
+    }
+    public static void main(String[] args) {
+        System.out.println("Введите стороны вашего треугольника: ");
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите сторону a: ");
+        int a  = scanner.nextInt();
+        System.out.println("Введите сторону b: ");
+        int b = scanner.nextInt();
+        System.out.println("Введите сторону c: ");
+        int c = scanner.nextInt();
+
+        System.out.println("Площадь тругольника (по формуле Геррона): " + ploshad(a, b, c));
+        System.out.println("Периметр треугольника: " + perimetr(a, b, c));
+        System.out.println("Треугольник является прямоугольным: " + pryamougolnyi(a,b,c));
+    }
+}
--- src/zadanie14/Treugolnik.java
+++ src/zadanie14/Treugolnik.java
@@ -15,18 +15,4 @@ public class Treugolnik
    {
        return a + b + c;
    }
-
-    public static void main(String[] args) {
-        System.out.println("Введите стороны вашего треугольника: ");
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Введите сторону a: ");
-        int a  = scanner.nextInt();
-        System.out.println("Введите сторону b: ");
-        int b = scanner.nextInt();
-        System.out.println("Введите сторону c: ");
-        int c = scanner.nextInt();
-
-        System.out.println("Площадь тругольника (по формуле Геррона): " + ploshad(a, b, c));
-        System.out.println("Периметр треугольника: " + perimetr(a, b, c));
-    }
 }
